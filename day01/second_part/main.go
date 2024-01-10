947936695d75612c9ed1cd89690053aaa014a7b8
AkifhanIlgaz <akifhanilgaz@gmail.com>
2024-01-10 11:31:10 +0300

    solve day 01 second part

--- day01/second_part/main.go
+++ day01/second_part/main.go
@@ -1,1 +1,72 @@
 package secondpart
+
+import (
+	"strings"
+	"unicode"
+
+	"github.com/AkifhanIlgaz/advent-of-code-2023/utils"
+)
+
+var spelledDigits = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
+func Solve(lines []string) int {
+	return getCalibrationValues(lines)
+}
+
+func getCalibrationValues(documents []string) int {
+	totalCalibrationValue := 0
+
+	for _, doc := range documents {
+		var (
+			firstDigit = getFirstDigit(doc)
+			lastDigit  = getLastDigit(doc)
+		)
+		totalCalibrationValue += firstDigit * 10
+		totalCalibrationValue += lastDigit
+	}
+
+	return totalCalibrationValue
+}
+
+func getFirstDigit(s string) int {
+	for i := 0; i < len(s); i++ {
+		if d, found := containsSpelledDigit(s[:i]); found {
+			return d
+		} else if unicode.IsDigit(rune(s[i])) {
+			return utils.StringDigitToInt(s[i])
+		}
+	}
+
+	panic("No digit found")
+}
+
+func getLastDigit(s string) int {
+	for i := len(s) - 1; i >= 0; i-- {
+		if d, found := containsSpelledDigit(s[i:]); found {
+			return d
+		} else if unicode.IsDigit(rune(s[i])) {
+			return utils.StringDigitToInt(s[i])
+		}
+	}
+
+	panic("No digit found")
+}
+
+func containsSpelledDigit(s string) (int, bool) {
+	for k, v := range spelledDigits {
+		if strings.Contains(s, k) {
+			return v, true
+		}
+	}
+	return 0, false
+}
--- day01/solve.go
+++ day01/solve.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	firstpart "github.com/AkifhanIlgaz/advent-of-code-2023/day01/first_part"
+	secondpart "github.com/AkifhanIlgaz/advent-of-code-2023/day01/second_part"
 	"github.com/AkifhanIlgaz/advent-of-code-2023/utils"
 )
 
@@ -11,4 +12,5 @@ func Solve() {
 	lines := utils.ReadLines("./day01/input.txt")
 
 	fmt.Println("Part 1: ", firstpart.Solve(lines))
+	fmt.Println("Part 2: ", secondpart.Solve(lines))
 }
